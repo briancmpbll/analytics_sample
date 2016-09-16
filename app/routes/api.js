9daadd50bf9874ec84b33254b78979ed17621656
Brian Campbell <briancmpbll@gmail.com>
2016-09-16 13:56:03 -0700

    Add error handling

--- app/routes/api.js
+++ app/routes/api.js
@@ -7,10 +7,24 @@ var router = express.Router();
 var request = require('request');
 var zlib = require('zlib');
 
+function sendError(res) {
+  res.status(500).json({ error: 'Something went wrong' });
+}
+
 router.get('/data', function(req, res) {
-  var read = request
-    .get('http://aware-ui-test1.s3.amazonaws.com/sample_data.json.txt.gz');
-  read.pipe(zlib.createGunzip()).pipe(res);
+  var get = request.get('http://aware-ui-test1.s3.amazonaws.com/sample_data.json.txt.gz');
+  get.on('response', function(response) {
+      if (response.statusCode === 200) {
+        get.pipe(zlib.createGunzip()).pipe(res);
+      }
+      else {
+        sendError(res);
+      }
+    })
+    .on('error', function(err) {
+      console.log(err);
+      sendError(res);
+    });
 });
 
 module.exports = router;
--- public/js/app.js
+++ public/js/app.js
@@ -11,8 +11,8 @@ app.config([
   '$urlRouterProvider',
   function($stateProvider, $urlRouterProvider) {
     $stateProvider.state('index', {
-      url: '/index',
-      templateUrl: '/templates/analytics.html',
+      url: '',
+      templateUrl: '/views/analytics.html',
       controller: 'AnalyticsController',
       controllerAs: 'ctrl',
       resolve: {
@@ -20,8 +20,27 @@ app.config([
           return Data.$promise;
         }
       }
+    })
+    .state('404', {
+      templateUrl: '/views/404.html'
+    })
+    .state('error', {
+      templateUrl: '/views/error.html'
     });
 
-    $urlRouterProvider.otherwise('index');
+    $urlRouterProvider.otherwise(function($injector, $location){
+       var state = $injector.get('$state');
+       state.go('404');
+       return $location.path();
+    });
+  }]);
+
+app.run([
+  '$rootScope',
+  '$state',
+  function($rootScope, $state) {
+    $rootScope.$on('$stateChangeError', function() {
+      $state.go('error');
+    });
   }]);
 })();
