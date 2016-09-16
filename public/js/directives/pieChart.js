a6e538bc1be945cd9c4bc3a61671421ac03ea238
Brian Campbell <briancmpbll@gmail.com>
2016-09-16 12:17:52 -0700

    Clean up pie chart rendering

--- public/js/directives/pieChart.js
+++ public/js/directives/pieChart.js
@@ -46,44 +46,47 @@ app.directive('pieChart', ['$filter', function($filter) {
           'translate(' + (width/2) + ',' + (pieHeight/2) + ')');
 
       // Declare the function to render dynamic elements.
-      ctrl.render = function() {
-        var paths = chart.selectAll('path')
-          .data(pie(ctrl.revenueByProduct));
-        var texts = chart.selectAll('text')
+      var render = function() {
+        var slices = chart.selectAll('.slice')
           .data(pie(ctrl.revenueByProduct));
 
-        // Merge new sections with existing sections and apply operations
-        // to both.
-        paths.enter().append('path')
+        // Add new slices.
+        slices.enter().append('g')
+          .attr('class', 'slice')
           .on('click', function(d) {
             ctrl.onProductClicked( {product: d.data.key} );
           })
-          .merge(paths)
-            .attr('d', arc)
-            .attr('fill', function(d) { return color(d.data.key); })
-            .attr('stroke', function(d) { return color(d.data.key); });
+          // Add slice children.
+          .each(function() {
+            var me = d3.select(this);
 
-        texts.enter().append('text')
-          .merge(texts)
-            .attr('transform', function(d) {
-              d.innerRadius = 0;
-              d.outerRadius = radius;
-              return 'translate(' + arc.centroid(d) + ')';
-            })
-            .attr('text-anchor', 'middle')
-            .text(function(d) {
-              return $filter('millions')(d.value);
-            });
+            me.append('path');
+            me.append('text');
+          })
+          // Merge with existing slices and update fields.
+          .merge(slices)
+          .each(function(d) {
+            var me = d3.select(this);
 
-        // Remove unecessary sections
-        paths.exit().remove();
-        texts.exit().remove();
+            me.select('path')
+              .attr('d', arc(d))
+              .attr('fill', color(d.data.key))
+              .attr('stroke', color(d.data.key));
+
+            me.select('text')
+              .attr('transform', 'translate(' + arc.centroid(d) + ')')
+              .attr('text-anchor', 'middle')
+              .text($filter('millions')(d.value));
+          });
+
+        // Remove old slices.
+        slices.exit().remove();
 
         // Add legend.
         var legend = svg.selectAll('.legend')
           .data(color.domain());
 
-        // Append new elements
+        // Append new legend rows.
         legend.enter().append('g')
           .attr('class', 'legend')
           .attr('transform', function(d, i) {
@@ -112,6 +115,7 @@ app.directive('pieChart', ['$filter', function($filter) {
               .text(function(d) { return d; });
           });
 
+        // Remove old legend rows.
         legend.exit().remove();
       };
 
@@ -125,7 +129,7 @@ app.directive('pieChart', ['$filter', function($filter) {
           })
           .entries(ctrl.data[ctrl.year]);
 
-        ctrl.render();
+        render();
       });
     }
   };
